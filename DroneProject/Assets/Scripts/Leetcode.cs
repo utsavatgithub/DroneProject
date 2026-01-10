845e465ba0addabefaa7945ed795d41c7f32314f
utsavatgithub <74044738+utsavatgithub@users.noreply.github.com>
2026-01-10 16:36:06 +0530

    Add files via upload

---
+++ DroneProject/Assets/Scripts/Leetcode.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Leetcode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
